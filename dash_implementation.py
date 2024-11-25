4bb4e2f994ba9f842b2e7bc5063e9c1959a5cb66
lorenzonebolosi <lorenzo.nebolosi@mail.polimi.it>
2024-11-25 17:57:12 +0100

    Fixed hover_template

--- dash_implementation.py
+++ dash_implementation.py
@@ -1,13 +1,10 @@
-
 import pandas as pd
 from dash import Output, Input, Dash, dcc, html
 import numpy as np
 import plotly.graph_objects as go
 
-
-
 data = pd.read_csv("tempTable.csv")
-data["lambda"] = 1./data["phi"]
+data["lambda"] = 1. / data["phi"]
 app = Dash(__name__)
 
 axis = ["p", "Tu", "phi", "egr"]
@@ -38,6 +35,21 @@ app.layout = html.Div([
             dcc.Dropdown(id='slice-dropdown', options=[{'label': ax, 'value': ax} for ax in axis], value=axis[3]),
             dcc.Slider(id='slice-slider', min=0, max=0, step=1, value=0, marks={}),
         ], style={'width': '100%', 'marginBottom': '10px'}),
+
+        # RadioItems for completed filter
+        html.Div([
+            html.Label('Completion Status:'),
+            dcc.RadioItems(
+                id='completion-filter',
+                options=[
+                    {'label': 'Completed', 'value': 'completed'},
+                    {'label': 'Not Completed', 'value': 'not_completed'},
+                    {'label': 'Both', 'value': 'both'}
+                ],
+                value='completed',  # Default to 'Completed'
+                inline=True
+            )
+        ], style={'width': '100%', 'marginBottom': '10px'}),
     ], style={'width': '25%', 'padding': '20px', 'display': 'flex', 'flexDirection': 'column'}),
 
     # Right container for the graph
@@ -53,13 +65,12 @@ app.layout = html.Div([
      Output('slice-slider', 'max'),
      Output('slice-slider', 'marks'),
      Output('slice-slider', 'value')],
-     Input('slice-dropdown', 'value')
+    Input('slice-dropdown', 'value')
 )
-
 def update_slice_slider(slice_axis):
     unique_values = ranges[slice_axis]
     marks = {i: str(v) for i, v in enumerate(unique_values)}
-    return 0, len(unique_values)-1, marks, 0
+    return 0, len(unique_values) - 1, marks, 0
 
 
 @app.callback(
@@ -68,26 +79,31 @@ def update_slice_slider(slice_axis):
      Input('yaxis-dropdown', 'value'),
      Input('zaxis-dropdown', 'value'),
      Input('slice-dropdown', 'value'),
-     Input('slice-slider', 'value')]
+     Input('slice-slider', 'value'),
+     Input('completion-filter', 'value')]  # Add the completion filter as an input
 )
-
-def update_graph(xaxis, yaxis, zaxis, slice_axis, slice_idx):
+def update_graph(xaxis, yaxis, zaxis, slice_axis, slice_idx, completed_filter):
     # Ensure all axes are unique
     if len({xaxis, yaxis, zaxis, slice_axis}) < 4:
         return go.Figure()
     waxis = [x for x in axis if x not in [xaxis, yaxis, zaxis]][0]
     # Get current slice value
     waxis_value = ranges[slice_axis][slice_idx]
-    #print(data.info)
-    # Filter data
-    print(xaxis,yaxis,zaxis,waxis,waxis_value)
-    subData = data[(data.loc[:,waxis] == waxis_value) & data["completed"]]
-    #print(subData.info)
+
+    # Filter data based on slice and completion status
+    if completed_filter == 'completed':
+        subData = data[(data.loc[:, waxis] == waxis_value) & (data["completed"] == True)]
+    elif completed_filter == 'not_completed':
+        subData = data[(data.loc[:, waxis] == waxis_value) & (data["completed"] == False)]
+    else:  # Both
+        subData = data[data.loc[:, waxis] == waxis_value]
+
     hover_template = (
-        f"<b>{xaxis}</b>: {{customdata[0]}}<br>"
-        f"<b>{yaxis}</b>: {{customdata[1]}}<br>"
-        f"<b>{zaxis}</b>: {{customdata[2]}}<br>"
-        f"<extra></extra>"
+        f'<b>{xaxis}</b>:' '%{customdata[0]}<br>'
+        f'<b>{yaxis}</b>:' '%{customdata[1]}<br>'
+        f'<b>{zaxis}</b>:' '%{customdata[2]}<br>'
+        f'<b>{waxis}</b>: {waxis_value}<br>'
+        f'<extra></extra>'
     )
     # Create 3D scatter plot
     fig = go.Figure(data=go.Scatter3d(
@@ -102,15 +118,9 @@ def update_graph(xaxis, yaxis, zaxis, slice_axis, slice_idx):
             colorbar=dict(title='Su'),
             opacity=0.8
         ),
-        customdata = np.stack([subData[xaxis], subData[yaxis], subData[zaxis]], axis=-1)
-        #hovertemplate=(
-        #    f"<b>{xaxis}</b>: {x}<br>"
-        #    f"<b>{yaxis}</b>: {y}<br>"
-        #    f"<b>{zaxis}</b>: {z}<br>"
-        #    f"<extra></extra>"
-        #)
+        customdata=np.stack([subData[xaxis], subData[yaxis], subData[zaxis]], axis=-1),
+        hovertemplate=hover_template
     ))
-
     fig.update_layout(
         scene=dict(
             xaxis_title=xaxis,
